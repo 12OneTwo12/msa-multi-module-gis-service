161ef36bf9dff1561870c33ca46d66affc4309bb
12OneTwo12 <jji042842@gmail.com>
2025-02-20 16:46:50 +0900

    default project setting and discovery service, api gateway done

---
+++ README.md
@@ -0,0 +1,1 @@
+# msa-multi-module-gis-service
---
+++ gis-api-gateway/src/test/java/com/onetwo/gisapigateway/GisApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetwo.gisapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
